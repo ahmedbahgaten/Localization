8004c95763c8a99f13d41b39bc265ae8d0a01f62
ahmedbahgaten <ahmedbahgatnabih@gmail.com>
2020-01-17 04:15:11 -0800

    Initial Commit

---
+++ localizationProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  localizationProject
+//
+//  Created by AHMED on 1/17/20.
+//  Copyright © 2020 AHMED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
